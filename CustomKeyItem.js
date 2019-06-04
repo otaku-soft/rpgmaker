cbb22f2a517abb656616c3e46c2d5c68c0cd6c4c
otaku-soft <flamehiro@gmail.com>
2019-06-03 20:56:20 -0600

    Add files via upload

--- CustomKeyItem.js
+++ CustomKeyItem.js
@@ -1,15 +1,9 @@
-Window_ItemList.prototype.drawItem = function(index) 
-{
-    var item = this._data[index];
-    if (item) 
+Window_ItemList.prototype.drawItemNumber = function(item, x, y, width) {
+    if (item['itypeId'] != 2)
     {
-        var numberWidth = this.numberWidth();
-        var rect = this.itemRect(index);
-        rect.width -= this.textPadding();
-        this.changePaintOpacity(this.isEnabled(item));
-        this.drawItemName(item, rect.x, rect.y, rect.width - numberWidth);
-        if (this['_category'] != "keyItem")
-            this.drawItemNumber(item, rect.x, rect.y, rect.width);
-        this.changePaintOpacity(1);
+        if (this.needsNumber()) {
+            this.drawText(':', x, y, width - this.textWidth('00'), 'right');
+            this.drawText($gameParty.numItems(item), x, y, width, 'right');
+        }
     }
 };
