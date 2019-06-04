72c0ef264a9c4a1c29e7efd2e34be3bbcb082cb9
otaku-soft <flamehiro@gmail.com>
2019-06-03 20:57:18 -0600

    Add files via upload

--- CustomKeyItem.js
+++ CustomKeyItem.js
@@ -2,7 +2,7 @@ Window_ItemList.prototype.drawItemNumber = function(item, x, y, width) {
     if (item['itypeId'] != 2)
     {
         if (this.needsNumber()) {
-            this.drawText(':', x, y, width - this.textWidth('00'), 'right');
+            this.drawText(' ', x, y, width - this.textWidth('00'), 'right');
             this.drawText($gameParty.numItems(item), x, y, width, 'right');
         }
     }
